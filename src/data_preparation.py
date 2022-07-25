4669d2f2b1b09ded188a49289c477d1976452480
Cristina Nogales Rodriguez <cn321@nvidia4.ma.ic.ac.uk>
2022-07-25 14:55:32 +0100

    basic gp start

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -7,12 +7,17 @@ from sklearn.model_selection import train_test_split
 
 
 def get_birth_data():
-    data = pd.read_csv('../data/birth_data.csv')
+    data = pd.read_csv('../../data/birth_data.csv')
     data['date'] = pd.to_datetime(data[['year', 'month', 'day']])
     data['ids'] = np.arange(1, data.shape[0]+1)
     data['weekday'] = data.day_of_week.apply(lambda x: 1 if x in [1,2,3,4,5] else 0)
     data['monday'] = data.day_of_week.apply(lambda x: 1 if x != 1 else 0)
     data['seasons'] = data.month.apply(set_season)
+
+    s = StandardScaler()
+    y = data.births
+    y = np.reshape(y.to_numpy(), (y.shape[0],1))
+    data['normalised_births'] = s.fit_transform(y)
 
     return data
 
@@ -43,21 +48,25 @@ def train_test_save(data):
     y_test = s.transform(y_test)
     df_test['births'] = y_test
 
-    df_train.to_csv("../data/train.csv", index=False)
-    df_test.to_csv("../data/test.csv", index=False)
+    df_train.to_csv("../../data/train.csv", index=False)
+    df_test.to_csv("../../data/test.csv", index=False)
 
     # incase we want to use them again later to convert back to original feature space
     scaling_params = pd.DataFrame({'Mean': s.mean_, 'Std': s.scale_})
     scaling_params.to_csv("../data/scaling_params.csv", index=False)
 
 
-def separate_data(data, weekdays=None):
+def separate_data(data, weekdays=None, train_test=1):
     if weekdays == 'weekdays':
         data = data.loc[data.weekday==1]
     if weekdays == 'weekends':
         data = data.loc[data.weekday==0]
+
     x = data.ids
-    y = data.births
+    if train_test == 1:
+        y = data.births
+    else:
+        y = data.normalised_births
     
     x = tf.cast(x, tf.float64)
     y = tf.cast(y, tf.float64)
