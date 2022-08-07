fa3e27dc194ddf63f701fa11d5a1ed74a0733c52
Cristina Nogales Rodriguez <cn321@nvidia4.ma.ic.ac.uk>
2022-08-07 10:38:12 +0100

    added CI percentage and traintest on trainable noise variance - notebook done!?!

---
+++ src/useful_fun.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_distribution(X, Y, mean, var, pos, labels):
+    plt.figure(figsize=(25, 7))
+
+    plt.plot(X, Y, '.', color="black", alpha=0.5, markersize=3)
+    plt.plot(X, mean, color="C0")
+    c = 1.96 * np.sqrt(var)
+    plt.fill_between(X[:,0], (mean - c)[:,0], (mean + c)[:,0], alpha=0.2, edgecolor='gray', facecolor='C0')
+
+    plt.xticks(pos, labels)
+    plt.xlabel('Date', fontsize=15)
+    plt.ylabel('Normalised Births', fontsize=15)
+    plt.tick_params(axis='both', which='major', labelsize=12)
+    plt.show()
+    plt.close()
+
+
+def percentage_outof_CI(Y, mean, var):
+    c = 1.96 * np.sqrt(var)
+    lower = mean - c
+    upper = mean + c
+    points_outof_bounds = (Y < lower) | (Y > upper)
+    return np.mean(points_outof_bounds) * 100
