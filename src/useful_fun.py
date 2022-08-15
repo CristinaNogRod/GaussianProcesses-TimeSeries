3932af173a019d1ae8f54c9df563c27af4402726
Cristina Nogales Rodriguez <cn321@nvidia4.ma.ic.ac.uk>
2022-08-16 00:12:41 +0100

    basic done, additive done, sliding window in progress

--- src/useful_fun.py
+++ src/useful_fun.py
@@ -4,24 +4,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_distribution(X, Y, mean, var, pos, labels, method='bayes', Z=None):
+def plot_distribution(X, Y, mean, var, pos, labels, legend=True):
     plt.figure(figsize=(25, 7))
 
     plt.plot(X, Y, '.', color="black", alpha=0.5, markersize=3, label='True (Test) Observation Samples')
     plt.plot(X, mean, color="C0", label='Mean Predictive Posterior')
     c = 1.96 * np.sqrt(var)
-    if method == 'freq':
-        plt.fill_between(X[:,0], (mean - c)[:,0], (mean + c)[:,0], alpha=0.2, edgecolor='gray', facecolor='C0', label='CI')
-    if method == 'bayes':
-        #Z = m.inducing_variable.Z.numpy()
-        plt.plot(Z, np.zeros_like(Z), "k|", color='grey', mew=2, label="Inducing Locations")
-        plt.fill_between(X[:,0], (mean - c)[:,0], (mean + c)[:,0], alpha=0.2, edgecolor='gray', facecolor='C0', label='CI')
+    plt.fill_between(X[:,0], (mean - c)[:,0], (mean + c)[:,0], alpha=0.2, edgecolor='gray', facecolor='C0', label='95% CI')
 
     plt.xticks(pos, labels)
     plt.xlabel('Date', fontsize=15)
     plt.ylabel('Normalised Births', fontsize=15)
     plt.tick_params(axis='both', which='major', labelsize=12)
-    plt.legend(prop={'size':14})
+    if legend: 
+        plt.legend(prop={'size':14})
+        
     plt.show()
     plt.close()
 
@@ -50,13 +47,13 @@ def plot_sliding_window(x_train, x_test, y_train, y_test, mean_train, mean_test,
 
     plt.plot(x_train, mean_train, '-', label='Mean Posterior for train data', c='C0', linewidth=3)
     c_train = 1.96 * np.sqrt(var_train) 
-    plt.fill_between(x_train[:,0], (mean_train - c_train)[:,0], (mean_train + c_train)[:,0], alpha=0.4, edgecolor='gray', facecolor='C0', label='CI for train')
+    plt.fill_between(x_train[:,0], (mean_train - c_train)[:,0], (mean_train + c_train)[:,0], alpha=0.4, edgecolor='gray', facecolor='C0', label='95% CI for train')
 
     plt.vlines(x_test[0], colors='grey', linestyles='dashed', ymin=np.min(y_train), ymax=np.max(y_train))
 
     c_test = 1.96 * np.sqrt(var_test) 
     plt.plot(x_test, mean_test, '-', label='Mean Predictive Posterior for test data', c='maroon', linewidth=3)
-    plt.fill_between(x_test[:,0], (mean_test - c_test)[:,0], (mean_test + c_test)[:,0], alpha=0.4, edgecolor='gray', facecolor='grey', label='CI for test')
+    plt.fill_between(x_test[:,0], (mean_test - c_test)[:,0], (mean_test + c_test)[:,0], alpha=0.4, edgecolor='gray', facecolor='grey', label='95% CI for test')
     
     plt.title('Sliding window for iteration ' + str(iteration))
     plt.xlabel('Date')
